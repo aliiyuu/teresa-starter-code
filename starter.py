f06dc40358f30e9a2b02225b5447914bbbf8fc42
alicia <112369864+aliiyuu@users.noreply.github.com>
2025-02-02 23:45:32 -0800

    Update starter.py

--- starter.py
+++ starter.py
@@ -1,10 +1,19 @@
 import subprocess
 import signal
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--iterations", help="Number of iterations to run the interface for", type=int)
+args = parser.parse_args()
+if args.iterations:
+   iterations = args.iterations
+else:
+    iterations = 500
 
 # Start both scripts
 processes = [
-    subprocess.Popen(["python3", "interface.py"]),
+    subprocess.Popen(["python3", "interface.py", f"{iterations}"]),
     subprocess.Popen(["python3", "sim.py"]),
 ]
 
